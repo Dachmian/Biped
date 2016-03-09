a679628484ff5f2192c07925b3072f74463cc825
= <anicic.damir91@gmail.com>
2016-03-09 16:33:17 +0100

    fikset impact i theta, fortsatt feil svar

--- Biped/BipedRobot/Gait.cs
+++ Biped/BipedRobot/Gait.cs
@@ -88,12 +88,16 @@ namespace BipedRobot
         {
             //first time running use rand to find a valid gait
             BRgait gait = new BRgait(biped.param);
-            gait.impact(0.2);
+            Console.WriteLine(gait.impactFirstLine(-0.2,0.2));
+            Console.WriteLine(gait.impactSecondLine(-0.2, 0.2));
+            Console.WriteLine(gait.impactThirdLine(-0.2, 0.2));
+            setPosture(ref gait);
+            verifyParameters(gait);
         }
         public static void setPosture(ref BRgait gait)
         {
             gait.gaitParam.intervalStart = -Math.PI / 4;
-            gait.gaitParam.intervalStart = Math.PI / 4;
+            gait.gaitParam.intervalEnd = Math.PI / 4;
         }
         public static void setParametersRandom(ref BRgait gait)
         {
@@ -104,8 +108,9 @@ namespace BipedRobot
         }
         public static bool verifyParameters(BRgait gait)
         {
-            double thetaDotAtTSquare = (-MathNet.Numerics.Integration.GaussLegendreRule.Integrate(gait.secondIntegral, gait.gaitParam.intervalStart, gait.gaitParam.intervalEnd, 2)) /
-                (1 - Math.Exp(-MathNet.Numerics.Integration.GaussLegendreRule.Integrate(gait.firstIntegral, gait.gaitParam.intervalStart, gait.gaitParam.intervalEnd, 2)) * Math.Pow(gait.impact(gait.gaitParam.intervalEnd), 2));
+            double thetaDotAtTSquare = (-MathNet.Numerics.Integration.GaussLegendreRule.Integrate(gait.secondIntegral, gait.gaitParam.intervalStart, gait.gaitParam.intervalEnd, 20)) /
+                (1 - Math.Exp(-MathNet.Numerics.Integration.GaussLegendreRule.Integrate(gait.firstIntegral, gait.gaitParam.intervalStart, gait.gaitParam.intervalEnd, 20)) 
+                * Math.Pow(gait.impactFirstLine(gait.gaitParam.intervalStart, gait.gaitParam.intervalEnd), 20));
             if (thetaDotAtTSquare < 0)
             {
                 return false;
@@ -126,8 +131,13 @@ namespace BipedRobot
         private Expression _beta;
         private Expression _gamma;
 
-        private Expression _impact;
+        private Expression _impactPosFirstLine;
+        private Expression _impactPosSecondLine;
+        private Expression _impactPosThirdLine;
 
+        private Expression _impactNegFirstLine;
+        private Expression _impactNegSecondLine;
+        private Expression _impactNegThirdLine;
         public BRVHC()
         {
             StreamReader fs = null;
@@ -161,10 +171,40 @@ namespace BipedRobot
             _gamma = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
             fs.Close();
 
-            fs = new StreamReader(@"../../../impact.txt");
+            fs = new StreamReader(@"../../../impactPosFirstLine.txt");
             temp = fs.ReadLine();
-            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)","");
-            _impact = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactPosFirstLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            fs.Close();
+
+            fs = new StreamReader(@"../../../impactPosSecondLine.txt");
+            temp = fs.ReadLine();
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactPosSecondLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            fs.Close();
+
+            fs = new StreamReader(@"../../../impactPosThirdLine.txt");
+            temp = fs.ReadLine();
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactPosThirdLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            fs.Close();
+
+            fs = new StreamReader(@"../../../impactNegFirstLine.txt");
+            temp = fs.ReadLine();
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactNegFirstLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            fs.Close();
+
+            fs = new StreamReader(@"../../../impactNegSecondLine.txt");
+            temp = fs.ReadLine();
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactNegSecondLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
+            fs.Close();
+
+            fs = new StreamReader(@"../../../impactNegThirdLine.txt");
+            temp = fs.ReadLine();
+            temp = temp.Replace("tan", "Tan").Replace("cos", "Cos").Replace("sin", "Sin").Replace("pow", "Pow").Replace("(double)", "");
+            _impactNegThirdLine = new Expression(temp.Substring(temp.IndexOf('=') + 1, temp.LastIndexOf(';') - temp.IndexOf('=') - 1));
             fs.Close();
         }
         public double q1(double theta)
@@ -331,31 +371,162 @@ namespace BipedRobot
             return (2 * (double)_gamma.Evaluate() / (double)_alpha.Evaluate());
         }
 
-        public double impact(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        public double impactPosFirstLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
         {
-            _impact.Parameters["theta"] = theta;
+            _impactPosFirstLine.Parameters["theta"] = theta;
             foreach (var pair in gaitParam)
             {
-                _impact.Parameters[pair.Key] = pair.Value;
+                _impactPosFirstLine.Parameters[pair.Key] = pair.Value;
             }
 
-            _impact.Parameters["g"] = physicalParam.g;
-            _impact.Parameters["m1"] = physicalParam.m1;
-            _impact.Parameters["m2"] = physicalParam.m2;
-            _impact.Parameters["m3"] = physicalParam.m3;
+            _impactPosFirstLine.Parameters["g"] = physicalParam.g;
+            _impactPosFirstLine.Parameters["m1"] = physicalParam.m1;
+            _impactPosFirstLine.Parameters["m2"] = physicalParam.m2;
+            _impactPosFirstLine.Parameters["m3"] = physicalParam.m3;
 
-            _impact.Parameters["l1"] = physicalParam.l1;
-            _impact.Parameters["l2"] = physicalParam.l2;
-            _impact.Parameters["l3"] = physicalParam.l3;
+            _impactPosFirstLine.Parameters["l1"] = physicalParam.l1;
+            _impactPosFirstLine.Parameters["l2"] = physicalParam.l2;
+            _impactPosFirstLine.Parameters["l3"] = physicalParam.l3;
 
-            _impact.Parameters["L1"] = physicalParam.L1;
-            _impact.Parameters["L2"] = physicalParam.L2;
-            _impact.Parameters["L3"] = physicalParam.L3;
+            _impactPosFirstLine.Parameters["L1"] = physicalParam.L1;
+            _impactPosFirstLine.Parameters["L2"] = physicalParam.L2;
+            _impactPosFirstLine.Parameters["L3"] = physicalParam.L3;
 
-            _impact.Parameters["J1"] = physicalParam.J1;
-            _impact.Parameters["J2"] = physicalParam.J2;
-            _impact.Parameters["J3"] = physicalParam.J3;
-            return (double)_impact.Evaluate();
+            _impactPosFirstLine.Parameters["J1"] = physicalParam.J1;
+            _impactPosFirstLine.Parameters["J2"] = physicalParam.J2;
+            _impactPosFirstLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactPosFirstLine.Evaluate();
+        }
+        public double impactPosSecondLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        {
+            _impactPosSecondLine.Parameters["theta"] = theta;
+            foreach (var pair in gaitParam)
+            {
+                _impactPosSecondLine.Parameters[pair.Key] = pair.Value;
+            }
+
+            _impactPosSecondLine.Parameters["g"] = physicalParam.g;
+            _impactPosSecondLine.Parameters["m1"] = physicalParam.m1;
+            _impactPosSecondLine.Parameters["m2"] = physicalParam.m2;
+            _impactPosSecondLine.Parameters["m3"] = physicalParam.m3;
+
+            _impactPosSecondLine.Parameters["l1"] = physicalParam.l1;
+            _impactPosSecondLine.Parameters["l2"] = physicalParam.l2;
+            _impactPosSecondLine.Parameters["l3"] = physicalParam.l3;
+
+            _impactPosSecondLine.Parameters["L1"] = physicalParam.L1;
+            _impactPosSecondLine.Parameters["L2"] = physicalParam.L2;
+            _impactPosSecondLine.Parameters["L3"] = physicalParam.L3;
+
+            _impactPosSecondLine.Parameters["J1"] = physicalParam.J1;
+            _impactPosSecondLine.Parameters["J2"] = physicalParam.J2;
+            _impactPosSecondLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactPosSecondLine.Evaluate();
+        }
+        public double impactPosThirdLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        {
+            _impactPosThirdLine.Parameters["theta"] = theta;
+            foreach (var pair in gaitParam)
+            {
+                _impactPosThirdLine.Parameters[pair.Key] = pair.Value;
+            }
+
+            _impactPosThirdLine.Parameters["g"] = physicalParam.g;
+            _impactPosThirdLine.Parameters["m1"] = physicalParam.m1;
+            _impactPosThirdLine.Parameters["m2"] = physicalParam.m2;
+            _impactPosThirdLine.Parameters["m3"] = physicalParam.m3;
+
+            _impactPosThirdLine.Parameters["l1"] = physicalParam.l1;
+            _impactPosThirdLine.Parameters["l2"] = physicalParam.l2;
+            _impactPosThirdLine.Parameters["l3"] = physicalParam.l3;
+
+            _impactPosThirdLine.Parameters["L1"] = physicalParam.L1;
+            _impactPosThirdLine.Parameters["L2"] = physicalParam.L2;
+            _impactPosThirdLine.Parameters["L3"] = physicalParam.L3;
+
+            _impactPosThirdLine.Parameters["J1"] = physicalParam.J1;
+            _impactPosThirdLine.Parameters["J2"] = physicalParam.J2;
+            _impactPosThirdLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactPosThirdLine.Evaluate();
+        }
+
+        public double impactNegFirstLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        {
+            _impactNegFirstLine.Parameters["theta"] = theta;
+            foreach (var pair in gaitParam)
+            {
+                _impactNegFirstLine.Parameters[pair.Key] = pair.Value;
+            }
+
+            _impactNegFirstLine.Parameters["g"] = physicalParam.g;
+            _impactNegFirstLine.Parameters["m1"] = physicalParam.m1;
+            _impactNegFirstLine.Parameters["m2"] = physicalParam.m2;
+            _impactNegFirstLine.Parameters["m3"] = physicalParam.m3;
+
+            _impactNegFirstLine.Parameters["l1"] = physicalParam.l1;
+            _impactNegFirstLine.Parameters["l2"] = physicalParam.l2;
+            _impactNegFirstLine.Parameters["l3"] = physicalParam.l3;
+
+            _impactNegFirstLine.Parameters["L1"] = physicalParam.L1;
+            _impactNegFirstLine.Parameters["L2"] = physicalParam.L2;
+            _impactNegFirstLine.Parameters["L3"] = physicalParam.L3;
+
+            _impactNegFirstLine.Parameters["J1"] = physicalParam.J1;
+            _impactNegFirstLine.Parameters["J2"] = physicalParam.J2;
+            _impactNegFirstLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactNegFirstLine.Evaluate();
+        }
+        public double impactNegSecondLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        {
+            _impactNegSecondLine.Parameters["theta"] = theta;
+            foreach (var pair in gaitParam)
+            {
+                _impactNegSecondLine.Parameters[pair.Key] = pair.Value;
+            }
+
+            _impactNegSecondLine.Parameters["g"] = physicalParam.g;
+            _impactNegSecondLine.Parameters["m1"] = physicalParam.m1;
+            _impactNegSecondLine.Parameters["m2"] = physicalParam.m2;
+            _impactNegSecondLine.Parameters["m3"] = physicalParam.m3;
+
+            _impactNegSecondLine.Parameters["l1"] = physicalParam.l1;
+            _impactNegSecondLine.Parameters["l2"] = physicalParam.l2;
+            _impactNegSecondLine.Parameters["l3"] = physicalParam.l3;
+
+            _impactNegSecondLine.Parameters["L1"] = physicalParam.L1;
+            _impactNegSecondLine.Parameters["L2"] = physicalParam.L2;
+            _impactNegSecondLine.Parameters["L3"] = physicalParam.L3;
+
+            _impactNegSecondLine.Parameters["J1"] = physicalParam.J1;
+            _impactNegSecondLine.Parameters["J2"] = physicalParam.J2;
+            _impactNegSecondLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactNegSecondLine.Evaluate();
+        }
+        public double impactNegThirdLine(double theta, Dictionary<string, double> gaitParam, BRParameters physicalParam)
+        {
+            _impactNegThirdLine.Parameters["theta"] = theta;
+            foreach (var pair in gaitParam)
+            {
+                _impactNegThirdLine.Parameters[pair.Key] = pair.Value;
+            }
+
+            _impactNegThirdLine.Parameters["g"] = physicalParam.g;
+            _impactNegThirdLine.Parameters["m1"] = physicalParam.m1;
+            _impactNegThirdLine.Parameters["m2"] = physicalParam.m2;
+            _impactNegThirdLine.Parameters["m3"] = physicalParam.m3;
+
+            _impactNegThirdLine.Parameters["l1"] = physicalParam.l1;
+            _impactNegThirdLine.Parameters["l2"] = physicalParam.l2;
+            _impactNegThirdLine.Parameters["l3"] = physicalParam.l3;
+
+            _impactNegThirdLine.Parameters["L1"] = physicalParam.L1;
+            _impactNegThirdLine.Parameters["L2"] = physicalParam.L2;
+            _impactNegThirdLine.Parameters["L3"] = physicalParam.L3;
+
+            _impactNegThirdLine.Parameters["J1"] = physicalParam.J1;
+            _impactNegThirdLine.Parameters["J2"] = physicalParam.J2;
+            _impactNegThirdLine.Parameters["J3"] = physicalParam.J3;
+            return (double)_impactNegThirdLine.Evaluate();
         }
     }
 
@@ -406,9 +577,17 @@ namespace BipedRobot
             return Math.Exp(a) * _vhc.twoTimesGammaDividedByAlpha(theta, _gaitParam.gaitparameters, _physicalParam);
         }
 
-        public double impact(double theta)
+        public double impactFirstLine(double thetaStart, double thetaEnd)
         {
-            return _vhc.impact(theta, _gaitParam.gaitparameters, _physicalParam);
+            return _vhc.impactNegFirstLine(thetaStart, _gaitParam.gaitparameters, _physicalParam)/_vhc.impactPosFirstLine(thetaEnd, _gaitParam.gaitparameters, _physicalParam);
+        }
+        public double impactSecondLine(double thetaStart, double thetaEnd)
+        {
+            return _vhc.impactNegSecondLine(thetaStart, _gaitParam.gaitparameters, _physicalParam)/ _vhc.impactPosSecondLine(thetaEnd, _gaitParam.gaitparameters, _physicalParam);
+        }
+        public double impactThirdLine(double thetaStart, double thetaEnd)
+        {
+            return _vhc.impactNegThirdLine(thetaStart, _gaitParam.gaitparameters, _physicalParam)/ _vhc.impactPosThirdLine(thetaEnd, _gaitParam.gaitparameters, _physicalParam);
         }
 
     }
